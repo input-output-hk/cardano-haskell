81373b14b0a0a00eab1eb523e486912c688922d7
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-10-20 11:17:10 +1000

    Add cabal-cache to Buildkite build

--- build.sh
+++ build.sh
@@ -2,14 +2,58 @@
 
 set -euo pipefail
 
+echo "--- Setup"
+
 git submodule update --init
+
+################################################################################
+# Cabal config
+
+cabal_args=()
+if [ -n "${CABAL_STORE_DIR:-}" ]; then
+  cabal_args+=("--store-dir=$CABAL_STORE_DIR")
+fi
+
+################################################################################
+# Caching
+
+restore_cache() {
+  test -n "$cache_args" || exit 0
+  echo "--- Restoring from cache..."
+  cabal-cache sync-from-archive "${cache_args[@]}" || true
+  echo
+  echo "Finished restoring from cache"
+}
+
+save_cache() {
+  echo "--- Saving to cache..."
+  cabal-cache sync-to-archive "${cache_args[@]}" || true
+  echo
+  echo "Finished saving to cache"
+}
+
+cache_args=()
+if [ -n "${CABAL_CACHE_ARCHIVE:-}" ]; then
+  cache_args=(--threads=16 --archive-uri="$CABAL_CACHE_ARCHIVE")
+  if [ -n "${CABAL_STORE_DIR:-}" ]; then
+    cache_args+=("--store-path=$CABAL_STORE_DIR")
+  fi
+  trap "status=\$?; save_cache; exit \$status" EXIT
+fi
+
+################################################################################
+# cardano-repo-tool
 
 cd cardano-repo-tool
 cabal update
-cabal install --constraint="base16-bytestring<1.0.0.0" --installdir=../bin --overwrite-policy=always
+restore_cache
+echo "--- Building cardano-repo-tool"
+cabal "${cabal_args[@]}" install --constraint="base16-bytestring<1.0.0.0" --installdir=../bin --overwrite-policy=always
 cd ..
 
 ./bin/cardano-repo-tool --version
+
+echo "+++ Updating repos"
 
 ./bin/cardano-repo-tool clone-repos
 
@@ -19,4 +63,10 @@ cd ..
 
 cabal update
 
-cabal build all -O0
+restore_cache
+
+################################################################################
+# Build
+
+echo "+++ Building cardano-haskell"
+cabal "${cabal_args[@]}" build all -O0
--- shell.nix
+++ shell.nix
@@ -1,9 +1,24 @@
 { pkgs ? import <nixpkgs> {}
 , compiler ? "ghc865"
+# Import Haskell.nix master as of 2020-10-13, just for building cabal-cache.
+, haskellNix ? import (builtins.fetchTarball {
+    url = "https://github.com/input-output-hk/haskell.nix/archive/ab2f5a9b6d8cb1b18810194d9f4a6208710c358c.tar.gz";
+    sha256 = "1j3zwrrmnigl44lqdiyywwhj5m0wb4v6bpl89v1wp31vxwyyf7rv";
+  }) {}
 }:
 
 with pkgs;
 
+let
+  cabal-cache = (haskellNix.pkgs.haskell-nix.hackage-package {
+    name = "cabal-cache";
+    version = "1.0.2.1";
+    compiler-nix-name = compiler;
+    index-state = "2020-10-06T21:19:33Z";
+    plan-sha256 = "02dw6bk4p6vydzs9js6nd7v9bjpv3pwf5ga77q1c9pr1v8ylbjsj";
+  }).components.exes.cabal-cache;
+
+in
 mkShell rec {
   name = "cardano-haskell-env";
   meta.platforms = lib.platforms.unix;
@@ -14,6 +29,7 @@ mkShell rec {
     ghc
     cabal-install
     pkgconfig
+    cabal-cache
   ] ++ lib.optional (!stdenv.isDarwin) git;
 
   libs = [
